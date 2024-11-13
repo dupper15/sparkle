042eea845710026dc1309f408c4a9022f435d3b7
Nhật Nguyễn Lê Tuấn <22521019@ms.uit.edu.vn>
2024-11-13 16:49:36 +0700

    Fix addpagebutton display, sai lầm nào cũng phải trả giá:>

--- frontend/src/pages/WorkplacePage/WorkplacePage.jsx
+++ frontend/src/pages/WorkplacePage/WorkplacePage.jsx
@@ -208,10 +208,10 @@ const WorkplacePage = () => {
 
   return (
     <DndContext onDragEnd={handleDragEnd}>
-      <div className='w-screen h-screen bg-no-repeat bg-cover bg-[#151318] flex flex-col scrollbar-hide overflow-hidden'>
+      <div className="w-screen h-screen bg-no-repeat bg-cover bg-[#151318] flex flex-col scrollbar-hide overflow-hidden">
         <WorkplaceHeader />
-        <div className='flex h-[calc(100%-60px)] w-screen scrollbar-hide'>
-          <div className='w-[80px] bg-black z-50 scrollbar-hide h-full text-white overflow-y-auto'>
+        <div className="flex h-[calc(100%-60px)] w-screen scrollbar-hide">
+          <div className="w-[80px] bg-black z-50 scrollbar-hide h-full text-white overflow-y-auto">
             {[
               { icon: <LuLayoutTemplate />, label: "Design", type: "design" },
               { icon: <LuShapes />, label: "Shape", type: "shape" },
@@ -231,24 +231,24 @@ const WorkplacePage = () => {
                 className={`${
                   show.name === label.toLowerCase() ? "bg-[#252627]" : ""
                 } w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-[#610BEF]`}>
-                <span className='text-2xl'>{icon}</span>
-                <span className='text-xs font-medium'>{label}</span>
+                <span className="text-2xl">{icon}</span>
+                <span className="text-xs font-medium">{label}</span>
               </div>
             ))}
           </div>
 
-          <div className='h-full w-[calc(100%-75px)]'>
+          <div className="h-full w-[calc(100%-75px)]">
             <div
               className={`${
                 show.status ? "py-5 -left-[350px]" : "px-8 left-[75px] py-5"
               } bg-[#252627] h-full fixed transition-all w-[350px] z-30 duration-500`}>
               <div
                 onClick={() => setShow({ name: "", status: true })}
-                className='flex absolute justify-center items-center bg-[#252627] w-[20px] -right-2 text-slate-300 top-[40%] cursor-pointer h-[100px] rounded-full'>
+                className="flex absolute justify-center items-center bg-[#252627] w-[20px] -right-2 text-slate-300 top-[40%] cursor-pointer h-[100px] rounded-full">
                 <MdKeyboardArrowLeft />
               </div>
               {state === "design" && (
-                <div className='grid grid-cols-2 gap-2'>
+                <div className="grid grid-cols-2 gap-2">
                   <TemplateDesign />
                 </div>
               )}
@@ -259,21 +259,21 @@ const WorkplacePage = () => {
               {state === "project" && <Project />}
               {state === "text" && (
                 <div>
-                  <div className='grid grid-cols-1 gap-2'>
-                    <div className='bg-[#3c3c3d] cursor-pointer font-bold p-3 text-white text-x1 rounded-sm'>
+                  <div className="grid grid-cols-1 gap-2">
+                    <div className="bg-[#3c3c3d] cursor-pointer font-bold p-3 text-white text-x1 rounded-sm">
                       <h2>Add a text</h2>
                     </div>
                   </div>
                 </div>
               )}
               {state === "image" && (
-                <div className='h-[88vh] overflow-x-auto flex justify-start items-start scrollbar-hide'>
+                <div className="h-[88vh] overflow-x-auto flex justify-start items-start scrollbar-hide">
                   <Image />
                 </div>
               )}
               {state === "background" && <Background />}
             </div>
-            <div className='flex flex-col items-center justify-start gap-8 m-8 overflow-y-auto h-[calc(100%-50px)] scrollbar-hide'>
+            <div className="flex flex-col items-center justify-start gap-8 m-8 overflow-y-auto h-[calc(100%-50px)] scrollbar-hide">
               {pages.map((pageData, index) => (
                 <Page
                   key={pageData.id}
@@ -292,7 +292,9 @@ const WorkplacePage = () => {
                   ref={(el) => (pageRef.current[index] = el)}
                 />
               ))}
-              <AddPageButton addPage={addPage} />
+              <div>
+                <AddPageButton addPage={addPage} />
+              </div>
             </div>
           </div>
           <ButtonMessage toggleChatBox={toggleChatBox} />
