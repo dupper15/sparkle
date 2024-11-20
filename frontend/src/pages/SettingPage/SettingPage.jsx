a61f6135641edb2630d1f8c83ba7ab0a0dd63bc8
Nhật Nguyễn Lê Tuấn <22521019@ms.uit.edu.vn>
2024-11-20 10:46:17 +0700

    Nothing

--- frontend/src/pages/SettingPage/SettingPage.jsx
+++ frontend/src/pages/SettingPage/SettingPage.jsx
@@ -3,21 +3,21 @@ import Footer from "../../components/SharedComponents/Footer/Footer.jsx";
 import SettingSideBar from "../../components/SettingSideBar/SettingSideBar.jsx";
 
 const SettingPage = () => {
-    return (
-        <div className="w-screen h-screen flex flex-col">
-            <header>
-                <Header />
-            </header>
-            <div className="flex h-[calc(100%-60px)] w-screen scrollbar-hide">
-                <div className="w-[80px] z-50 scrollbar-hide h-full overflow-y-auto">
-                    <SettingSideBar />
-                </div>
-            </div>
-            <footer>
-                <Footer></Footer>
-            </footer>
+  return (
+    <div className="w-screen h-screen flex flex-col">
+      <header>
+        <Header />
+      </header>
+      <div className="flex h-[calc(100%-60px)] w-screen scrollbar-hide">
+        <div className="w-[80px] z-50 scrollbar-hide h-full overflow-y-auto">
+          <SettingSideBar />
         </div>
-    );
+      </div>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
 };
 
 export default SettingPage;
