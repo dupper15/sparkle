5215697c32240eb7ce2da317ac80b78f0710f1d5
highlamjay <caoduonglam61@gmail.com>
2024-11-23 20:50:26 +0700

    feat: refresh_token UI

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -5,15 +5,14 @@ dotenv.config()
 const authMiddleware = (req, res, next) => {
     const token = req.headers.token.split(' ')[1]
     const userId = req.params.id
-    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded){
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user){
         if (err){
             return res.status(404).json({           
                 status: 'ERROR',
                 message: 'The authentication'
             })
         }
-        const { payload } = decoded
-        if (decoded?.id === userId) {
+        if (user?.id === userId) {
             next()
         } else {
             return res.status(404).json({
