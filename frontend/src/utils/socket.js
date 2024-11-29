ea602ee5b6911cf89d5980fa7399d918222b631b
dupper15 <nguyenchinghia15@gmail.com>
2024-11-29 22:57:38 +0700

    hope

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,15 +1,19 @@
 import { io } from "socket.io-client";
+
 const socket = io("http://localhost:5001", {
   withCredentials: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
 });
+
 socket.on("connect", () => {
   console.log("Connected to Socket.IO server");
 });
+
 socket.on("connect_error", (error) => {
   console.error("Connection failed:", error);
 });
+
 socket.on("reconnect_attempt", () => {
   console.log("Attempting to reconnect...");
 });
