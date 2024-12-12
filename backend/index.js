88a79d379c96d8568bbc2152e4a23b691ee3241c
dupper15 <nguyenchinghia15@gmail.com>
2024-12-12 23:41:35 +0700

    feat: show cursor of different user in project

--- backend/index.js
+++ backend/index.js
@@ -6,25 +6,14 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { createServer } = require("http");
-const { Server } = require("socket.io");
-const { MongoClient } = require("mongodb");
-const { createAdapter } = require("@socket.io/mongo-adapter");
-const { sendMessage, getMessage } = require("./services/MessageService");
-const { textToImage } = require("./chatBot/textToImage");
+const initializeSocket = require("./sockets/socket");
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
 
 const server = createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5000",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
 
 app.use(
   cors({
@@ -34,105 +23,9 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(cookieParser());
-const DB_NAME = "sparkle_db";
-const COLLECTION_NAME = "socket.io-adapter-events";
-const mongoUri = process.env.MONGO_URI;
-
-const mongoClient = new MongoClient(mongoUri, {});
-
-(async () => {
-  try {
-    await mongoClient.connect();
-    const mongoCollection = mongoClient.db(DB_NAME).collection(COLLECTION_NAME);
-    io.adapter(createAdapter(mongoCollection));
-
-    console.log("MongoDB Adapter is set up for Socket.IO!");
-  } catch (error) {
-    console.error("Error setting up MongoDB Adapter:", error);
-  }
-})();
-const rooms = {};
-io.on("connection", (socket) => {
-  socket.on("setUserId", (userId) => {
-    socket.userId = userId;
-  });
-
-  socket.on("joinRoom", async (roomId) => {
-    socket.join(roomId);
-    if (!rooms[roomId]) {
-      rooms[roomId] = [];
-    }
-    if (!rooms[roomId].includes(socket.userId)) {
-      rooms[roomId].push(socket.userId);
-    }
-
-    io.to(roomId).emit("userInRoom", rooms[roomId]);
-    console.log("userInRoom", rooms[roomId]);
-
-    try {
-      const response = await getMessage(roomId);
-      socket.emit("loadMessages", response.data);
-    } catch (error) {
-      console.error("Error loading messages:", error);
-    }
-  });
-  socket.on("botReply", async (data) => {
-    const { botAnswer, roomId } = data;
-    try {
-      const response = await sendMessage({
-        content: botAnswer,
-        sender: "SparkleBot",
-        groupId: roomId,
-      });
-      io.to(roomId).emit("chatMessage", response.data);
-    } catch (error) {
-      console.error("Error sending message:", error);
-    }
-  });
-  socket.on("imageReply", async (data) => {
-    const { imageAnswer, roomId } = data;
-    try {
-      const response = await sendMessage({
-        imageUrl: imageAnswer,
-        content: "Đây là hình theo yêu cầu của bạn",
-        sender: "SparkleBot",
-        groupId: roomId,
-      });
-      io.to(roomId).emit("chatMessage", response.data);
-    } catch (error) {
-      console.error("Error sending message:", error);
-    }
-  });
-  socket.on("chatMessage", async (data) => {
-    const { userId, roomId, text, imageUrl } = data;
-
-    try {
-      const response = await sendMessage({
-        content: text,
-        sender: userId,
-        groupId: roomId,
-        imageUrl,
-      });
-      io.to(roomId).emit("chatMessage", response.data);
-    } catch (error) {
-      console.error("Error sending message:", error);
-    }
-  });
-
-  socket.on("disconnect", () => {
-    for (const roomId of Object.keys(rooms)) {
-      const index = rooms[roomId].indexOf(socket.userId);
-      if (index !== -1) {
-        rooms[roomId].splice(index, 1);
-        io.to(roomId).emit("userInRoom", rooms[roomId]);
-        console.log(`User ${socket.userId} removed from room ${roomId}`);
-      }
-    }
-    console.log("A user disconnected:", socket.userId);
-  });
-});
 
 routes(app);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -142,6 +35,8 @@ mongoose
     console.log(err);
   });
 
-server.listen(port, async () => {
+initializeSocket(server, process.env.MONGO_URI);
+
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
--- frontend/src/components/Canvas/CanvasViewModel.js
+++ frontend/src/components/Canvas/CanvasViewModel.js
@@ -1,137 +1,142 @@
-import {useEffect, useRef, useState} from "react";
-import {useDroppable} from "@dnd-kit/core";
-import {useDarkMode} from "../../contexts/DarkModeContext";
+import { useEffect, useRef, useState } from "react";
+import { useDroppable } from "@dnd-kit/core";
+import { useDarkMode } from "../../contexts/DarkModeContext";
 import axios from "axios";
-import {removeAndPopComponentFromCanvas} from "../../services/utils/componentOrchestrator.js";
+import { removeAndPopComponentFromCanvas } from "../../services/utils/componentOrchestrator.js";
 import ComponentService from "../../services/ComponentService.js";
 
 const useCanvasViewModel = (id, databaseId) => {
-    const [selectedComponentId, setSelectedComponentId] = useState(null);
-    const [isImageToolBarOpen, setOpenImageToolBar] = useState(false);
-    const [isTextToolBarOpen, setOpenTextToolBar] = useState(false);
-    const [shapes, setShapes] = useState([]); // Initialize with an empty array
-    const [selectedComponents, setSelectedComponents] = useState([]);
+  const [selectedComponentId, setSelectedComponentId] = useState(null);
+  const [isImageToolBarOpen, setOpenImageToolBar] = useState(false);
+  const [isTextToolBarOpen, setOpenTextToolBar] = useState(false);
+  const [shapes, setShapes] = useState([]);
+  const [selectedComponents, setSelectedComponents] = useState([]);
+  const canvasRef = useRef(null);
+  const { isOver, setNodeRef } = useDroppable({ id });
+  const { isDarkMode } = useDarkMode();
 
-    const canvasRef = useRef(null);
-    const {isOver, setNodeRef} = useDroppable({id});
-    const {isDarkMode} = useDarkMode();
+  const handleColorChange = (color) => {
+    setShapes((prevShapes) =>
+      prevShapes.map((shape) =>
+        selectedComponents.includes(shape._id) ? { ...shape, color } : shape
+      )
+    );
+    selectedComponents.forEach((componentId) => {
+      ComponentService.updateComponentColor("shape", color, componentId).then();
+    });
+  };
 
-    const handleColorChange = (color) => {
-        setShapes((prevShapes) =>
-            prevShapes.map((shape) =>
-                selectedComponents.includes(shape._id) ? { ...shape, color } : shape
-            )
+  const handleSelectComponent = (componentId) => {
+    setSelectedComponents((prev) => {
+      if (!prev.includes(componentId)) {
+        return [...prev, componentId];
+      }
+      return prev;
+    });
+  };
+
+  const handleImageClick = (shapeId) => {
+    setOpenImageToolBar(true);
+    setOpenTextToolBar(false);
+    handleSelectComponent(shapeId);
+  };
+
+  const handleTextClick = (databaseId) => {
+    setOpenTextToolBar(true);
+    setOpenImageToolBar(false);
+    setSelectedComponentId(databaseId);
+  };
+
+  const handleSendBackward = () => {
+    console.log("Send Backward");
+  };
+
+  const handleSendToBack = () => {
+    console.log("Send To Back");
+  };
+
+  const handleSendForward = () => {
+    console.log("Send Forward");
+  };
+
+  const handleSendToFront = () => {
+    console.log("Send To Front");
+  };
+
+  const removeComponent = async (componentId, componentType) => {
+    try {
+      await removeAndPopComponentFromCanvas(
+        databaseId,
+        componentType,
+        componentId
+      );
+      setShapes((prevShapes) => {
+        return prevShapes.filter((shape) => shape._id !== componentId);
+      });
+    } catch (error) {
+      console.error("Failed to remove component:", error);
+    }
+  };
+
+  // useEffect(() => {
+  //     const handleClickOutside = (event) => {
+  //         if (canvasRef.current && canvasRef.current.contains(event.target)) {
+  //             const isComponent = selectedComponents.some(componentId => {
+  //                 const componentElement = document.getElementById(`component-${componentId}`);
+  //                 return componentElement && componentElement.contains(event.target);
+  //             });
+  //
+  //             if (!isComponent) {
+  //                 setOpenImageToolBar(false);
+  //                 setOpenTextToolBar(false);
+  //                 setSelectedComponents([]);
+  //                 console.log(selectedComponents);
+  //             }
+  //         }
+  //     };
+  //
+  //     document.addEventListener("mousedown", handleClickOutside);
+  //     return () => {
+  //         document.removeEventListener("mousedown", handleClickOutside);
+  //     };
+  // }, [selectedComponents]);
+
+  useEffect(() => {
+    const fetchComponents = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_KEY}/canvas/get-components/${databaseId}`
         );
-        selectedComponents.forEach(componentId => {
-            ComponentService.updateComponentColor("shape", color, componentId).then();
-        });
-    }
-
-    const handleSelectComponent = (componentId) => {
-        setSelectedComponents((prev) => {
-            if (!prev.includes(componentId)) {
-                return [...prev, componentId];
-            }
-            return prev;
-        });
-    }
-
-    const handleImageClick = (shapeId) => {
-        setOpenImageToolBar(true);
-        setOpenTextToolBar(false);
-        handleSelectComponent(shapeId);
+        setShapes(response.data.data || []);
+      } catch (error) {
+        console.error("Failed to fetch components:", error);
+      }
     };
-
-    const handleTextClick = (databaseId) => {
-        setOpenTextToolBar(true);
-        setOpenImageToolBar(false);
-        setSelectedComponentId(databaseId);
+    fetchComponents();
+    document.addEventListener(`update-shapes-${id}`, fetchComponents);
+    return () => {
+      document.removeEventListener(`update-shapes-${id}`, fetchComponents);
     };
+  }, [databaseId, id]);
 
-    const handleSendBackward = () => {
-        console.log("Send Backward");
-    }
-
-    const handleSendToBack = () => {
-        console.log("Send To Back");
-    }
-
-    const handleSendForward = () => {
-        console.log("Send Forward");
-    }
-
-    const handleSendToFront = () => {
-        console.log("Send To Front");
-    }
-
-    const removeComponent = async (componentId, componentType) => {
-        try {
-            await removeAndPopComponentFromCanvas(databaseId, componentType, componentId);
-            setShapes((prevShapes) => {
-                return prevShapes.filter((shape) => shape._id !== componentId);
-            });
-        } catch (error) {
-            console.error("Failed to remove component:", error);
-        }
-    };
-
-    // useEffect(() => {
-    //     const handleClickOutside = (event) => {
-    //         if (canvasRef.current && canvasRef.current.contains(event.target)) {
-    //             const isComponent = selectedComponents.some(componentId => {
-    //                 const componentElement = document.getElementById(`component-${componentId}`);
-    //                 return componentElement && componentElement.contains(event.target);
-    //             });
-    //
-    //             if (!isComponent) {
-    //                 setOpenImageToolBar(false);
-    //                 setOpenTextToolBar(false);
-    //                 setSelectedComponents([]);
-    //                 console.log(selectedComponents);
-    //             }
-    //         }
-    //     };
-    //
-    //     document.addEventListener("mousedown", handleClickOutside);
-    //     return () => {
-    //         document.removeEventListener("mousedown", handleClickOutside);
-    //     };
-    // }, [selectedComponents]);
-
-    useEffect(() => {
-        const fetchComponents = async () => {
-            try {
-                const response = await axios.get(`${import.meta.env.VITE_API_KEY}/canvas/get-components/${databaseId}`);
-                setShapes(response.data.data || []);
-            } catch (error) {
-                console.error("Failed to fetch components:", error);
-            }
-        };
-        fetchComponents();
-        document.addEventListener(`update-shapes-${id}`, fetchComponents);
-        return () => {
-            document.removeEventListener(`update-shapes-${id}`, fetchComponents);
-        };
-    }, [databaseId, id]);
-
-    return {
-        selectedComponentId,
-        isImageToolBarOpen,
-        isTextToolBarOpen,
-        canvasRef,
-        isOver,
-        setNodeRef,
-        isDarkMode,
-        handleImageClick,
-        handleTextClick,
-        shapes,
-        removeComponent,
-        handleColorChange,
-        handleSendBackward,
-        handleSendToBack,
-        handleSendForward,
-        handleSendToFront
-    };
+  return {
+    selectedComponentId,
+    isImageToolBarOpen,
+    isTextToolBarOpen,
+    canvasRef,
+    isOver,
+    setNodeRef,
+    isDarkMode,
+    handleImageClick,
+    handleTextClick,
+    shapes,
+    removeComponent,
+    handleColorChange,
+    handleSendBackward,
+    handleSendToBack,
+    handleSendForward,
+    handleSendToFront,
+  };
 };
 
 export default useCanvasViewModel;
