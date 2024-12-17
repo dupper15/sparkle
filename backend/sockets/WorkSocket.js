de8651cf5bf13299383371d12131227ead53a80f
dupper15 <nguyenchinghia15@gmail.com>
2024-12-17 17:55:35 +0700

    feat: textSocket

--- backend/sockets/WorkSocket.js
+++ backend/sockets/WorkSocket.js
@@ -2,5 +2,35 @@ const WorkSocket = (socket, io) => {
   socket.on("componentColorChanged", ({ componentId, color, roomId }) => {
     socket.to(roomId).emit("componentColorChanged", { componentId, color });
   });
+  socket.on("textFontFamilyChanged", ({ componentId, fontFamily, roomId }) => {
+    socket
+      .to(roomId)
+      .emit("textFontFamilyChanged", { componentId, fontFamily });
+  });
+  socket.on("textFontSizeChanged", ({ componentId, fontSize, roomId }) => {
+    socket.to(roomId).emit("textFontSizeChanged", { componentId, fontSize });
+  });
+  socket.on("textFontWeightChanged", ({ componentId, fontWeight, roomId }) => {
+    socket
+      .to(roomId)
+      .emit("textFontWeightChanged", { componentId, fontWeight });
+  });
+  socket.on("textFontStyleChanged", ({ componentId, fontStyle, roomId }) => {
+    socket.to(roomId).emit("textFontStyleChanged", { componentId, fontStyle });
+  });
+  socket.on(
+    "textDecorationLineChanged",
+    ({ componentId, textDecorationLine, roomId }) => {
+      socket
+        .to(roomId)
+        .emit("textDecorationLineChanged", { componentId, textDecorationLine });
+    }
+  );
+  socket.on("textAlignChanged", ({ componentId, textAlign, roomId }) => {
+    socket.to(roomId).emit("textAlignChanged", { componentId, textAlign });
+  });
+  socket.on("textContentChanged", ({ componentId, content, roomId }) => {
+    socket.to(roomId).emit("textContentChanged", { componentId, content });
+  });
 };
 module.exports = WorkSocket;
