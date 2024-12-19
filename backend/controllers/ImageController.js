3543cb4913055547dde020b1a7934af577c36e2a
dupper15 <nguyenchinghia15@gmail.com>
2024-12-20 00:22:21 +0700

    feat: edit image function

--- backend/controllers/ImageController.js
+++ backend/controllers/ImageController.js
@@ -1,101 +1,113 @@
-const ImageService = require('../services/ImageService')
+const ImageService = require("../services/ImageService");
 
 const createImageUpload = async (req, res) => {
-    try {
-        console.log("req.body", req.body)
-        const response = await ImageService.createImageUpload(req.body)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
-    }
-}
+  try {
+    console.log("req.body", req.body);
+    const response = await ImageService.createImageUpload(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const getAllImage = async (req, res) => {
-    try {
-        const userId = req.params.id
-        const response = await ImageService.getAllImage(userId)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
-    }
-}
+  try {
+    const userId = req.params.id;
+    const response = await ImageService.getAllImage(userId);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const createImage = async (req, res) => {
-    try {
-        const response = await ImageService.createImage(req.body)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
-    }
-}
+  try {
+    const response = await ImageService.createImage(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const getDetailImage = async (req, res) => {
-    try {
-        const imageId = req.params.id
-        if (!imageId){
-            return res.status(400).json({
-                status: 'ERROR',
-                message: 'Id is not defined'
-            })
-        }
-        const response = await ImageService.getDetailImage(imageId)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
+  try {
+    const imageId = req.params.id;
+    if (!imageId) {
+      return res.status(400).json({
+        status: "ERROR",
+        message: "Id is not defined",
+      });
     }
-}
+    const response = await ImageService.getDetailImage(imageId);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const updateImage = async (req, res) => {
-    try {
-        const imageId = req.params.id
-        const data = req.body
-        if (!imageId){
-            return res.status(400).json({
-                status: 'ERROR',
-                message: 'Id is not defined'
-            })
-        }
-        const response = await ImageService.updateImage(imageId, data)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
+  try {
+    const imageId = req.params.id;
+    const data = req.body;
+    if (!imageId) {
+      return res.status(400).json({
+        status: "ERROR",
+        message: "Id is not defined",
+      });
     }
-}
+    const response = await ImageService.updateImage(imageId, data);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
+const removeBackground = async (req, res) => {
+  try {
+    const imageUrl = req.body;
+    const response = await ImageService.removeBackground(imageUrl);
 
+    console.log("hế 3", response);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 const deleteImage = async (req, res) => {
-    try {
-        const imageId = req.params.id
-        if (!imageId){
-            return res.status(400).json({
-                status: 'ERROR',
-                message: 'Id is not defined'
-            })
-        }
-        const response = await ImageService.deleteImage(imageId)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
+  try {
+    const imageId = req.params.id;
+    if (!imageId) {
+      return res.status(400).json({
+        status: "ERROR",
+        message: "Id is not defined",
+      });
     }
-}
-
+    const response = await ImageService.deleteImage(imageId);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 module.exports = {
-    createImageUpload,
-    createImage,
-    getAllImage,
-    getDetailImage,
-    updateImage,
-    deleteImage
-}
+  createImageUpload,
+  createImage,
+  getAllImage,
+  getDetailImage,
+  updateImage,
+  removeBackground,
+  deleteImage,
+};
--- backend/routes/ImageRouter.js
+++ backend/routes/ImageRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const router = express.Router()
-const imageController = require('../controllers/ImageController')
+const router = express.Router();
+const imageController = require("../controllers/ImageController");
 
-router.post('/create-upload', imageController.createImageUpload)
-router.get('/getAll/:id', imageController.getAllImage)
-router.post('/create', imageController.createImage)
-router.get('/get-detail/:id', imageController.getDetailImage)
-router.put('/update/:id', imageController.updateImage)
-router.delete('/delete/:id', imageController.deleteImage)
+router.post("/create-upload", imageController.createImageUpload);
+router.get("/getAll/:id", imageController.getAllImage);
+router.post("/create", imageController.createImage);
+router.get("/get-detail/:id", imageController.getDetailImage);
+router.put("/update/:id", imageController.updateImage);
+router.post("/remove-background", imageController.removeBackground);
+router.delete("/delete/:id", imageController.deleteImage);
 
-
-module.exports = router
+module.exports = router;
