5b28876c4c56fb5dc0c9297fc37930d370db8aed
Nhật Nguyễn Lê Tuấn <22521019@ms.uit.edu.vn>
2024-12-28 17:55:54 +0700

    Loading public/private

--- frontend/src/components/AddEditorForm/SettingWork.jsx
+++ frontend/src/components/AddEditorForm/SettingWork.jsx
@@ -5,9 +5,15 @@ import { useMutation } from "@tanstack/react-query";
 import * as ProjectService from "../../services/ProjectService";
 import Avatar from "antd/es/avatar/avatar";
 import profileIcon from "../../assets/default-profile-icon.png";
-import * as Message from "../Alert/Alert"
+import * as Message from "../Alert/Alert";
 
-const SettingWork = ({ status, handlePrivate, handlePublic, setOpenAddEditor }) => {
+const SettingWork = ({
+  isLoading,
+  status,
+  handlePrivate,
+  handlePublic,
+  setOpenAddEditor,
+}) => {
   const [items, setItems] = useState([]);
   const projecId = localStorage.getItem("projectId");
   const [refresh, setRefresh] = useState(false);
@@ -18,10 +24,9 @@ const SettingWork = ({ status, handlePrivate, handlePublic, setOpenAddEditor }) 
     },
     onSuccess: (data) => {
       console.log("Project updated successfully:", data);
-      setItems(data.data)
-
+      setItems(data.data);
     },
-  })
+  });
 
   const mutationDelete = useMutation({
     mutationFn: (data) => {
@@ -29,57 +34,52 @@ const SettingWork = ({ status, handlePrivate, handlePublic, setOpenAddEditor }) 
     },
     onSuccess: (data) => {
       console.log("Project updated successfully:", data);
-      setRefresh(!refresh)
-      Message.success("Remove editor successfully")
+      setRefresh(!refresh);
+      Message.success("Remove editor successfully");
       setOpenAddEditor();
     },
-  })
+  });
 
   useEffect(() => {
-    mutationGet.mutate(projecId)
-  }, [refresh])
+    mutationGet.mutate(projecId);
+  }, [refresh]);
 
-  const handleRemove = (email) => { 
-    mutationDelete.mutate({ id: projecId, email: email})
+  const handleRemove = (email) => {
+    mutationDelete.mutate({ id: projecId, email: email });
   };
 
   return (
-    <div className="bg-gray-50 dark:bg-gray-900 p-6 rounded-lg shadow-md w-[240px] mx-auto">
+    <div className="bg-gray-50 dark:bg-gray-900 p-6 rounded-lg shadow-md w-[300px] mx-auto">
       {/* Section: Privacy Toggle */}
       <div className="flex items-center justify-between gap-2 mb-6">
         <button
+          disabled={isLoading}
           onClick={status ? handlePrivate : handlePublic}
           className="w-full h-[40px] bg-indigo-600 gap-4 text-white font-semibold rounded-lg shadow cursor-pointer flex justify-center items-center hover:bg-indigo-400 transition-colors">
           <MdOutlinePublic size={24} className="text-white" />
-          {status ? "Private" : "Public"}
+          {isLoading ? "Loading..." : status ? "Private" : "Public"}
         </button>
       </div>
 
       {/* Section: User List */}
-      <div className='flex flex-col gap-3'>
-        {items.length === 0 ? (
-          // Nếu items rỗng, hiển thị div trắng
-          <div className='h-20 bg-white rounded-lg shadow'></div>
-        ) : (
-          // Nếu items không rỗng, hiển thị danh sách
-          items.map((item, index) => (
-            <div
-              key={index}
+      <div className="flex flex-col gap-3">
+        {items.map((item, index) => (
+          <div
+            key={index}
             className="flex items-center justify-between px-2 py-4 bg-white dark:bg-black rounded-lg shadow hover:bg-gray-100 transition-colors">
-              <div className='flex items-center gap-3'>
-                <Avatar size={40} src={item.image || profileIcon} />
+            <div className="flex items-center gap-3">
+              <Avatar size={40} src={item.image || profileIcon} />
               <span className="text-sm font-medium text-gray-700 dark:text-white">
-                  {item.userName}
-                </span>
-              </div>
-              <RiDeleteBin6Line
-                onClick={() => handleRemove(item.email)}
-                size={20}
-                className='text-gray-500 dark:text-white cursor-pointer dark:hover:text-red-500 hover:text-red-500 transition-colors'
-              />
+                {item.userName}
+              </span>
             </div>
-          ))
-        )}
+            <RiDeleteBin6Line
+              onClick={() => handleRemove(item.email)}
+              size={20}
+              className="text-gray-500 dark:text-white cursor-pointer dark:hover:text-red-500 hover:text-red-500 transition-colors"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
